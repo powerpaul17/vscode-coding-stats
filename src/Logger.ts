a4d175f0c8117934ece948b2549c45e3769e770b
Paul Tirk <paultirk@paultirk.com>
2021-09-09 18:29:52 +0200

    write log output also to output channel

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,25 +1,45 @@
+import {OutputChannel, workspace} from 'vscode';
+
 export class Logger {
 
-  private static prefix = 'coding-stats';
+  private static readonly prefix = 'Coding Stats';
+
+  private static outputChannel: OutputChannel|null = null;
+
+  public static setOutputChannel(outputChannel: OutputChannel): void {
+    this.outputChannel = outputChannel;
+  }
 
   public static error(...msg: Array<any>): void {
     console.error(this.prefix, ...msg);
+    this.writeToOutputChannel([ 'ERROR:', ...msg]);
   }
 
   public static warn(...msg: Array<any>): void {
     console.warn(this.prefix, ...msg);
+    this.writeToOutputChannel([ 'WARN:', ...msg]);
   }
 
   public static info(...msg: Array<any>): void {
     console.info(this.prefix, ...msg);
+    this.writeToOutputChannel([ 'INFO:', ...msg]);
   }
 
   public static log(...msg: Array<any>): void {
     console.log(this.prefix, ...msg);
+    this.writeToOutputChannel([ 'LOG:', ...msg ]);
   }
 
   public static debug(...msg: Array<any>): void {
-    console.debug(this.prefix, ...msg);
+    const configuration = workspace.getConfiguration('codingStats');
+    if(configuration.get('debugLogging')) {
+      console.debug(this.prefix, ...msg);
+      this.writeToOutputChannel([ 'DEBUG:', ...msg]);
+    }
+  }
+
+  private static writeToOutputChannel(msg: Array<any>): void {
+    this.outputChannel?.appendLine(msg.join(' '));
   }
 
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import {ExtensionContext} from 'vscode';
 import {EventHandler} from './EventHandler';
+import {Logger} from './Logger';
 import {Uploader} from './Uploader';
 
 const uploader = new Uploader();
@@ -8,7 +9,7 @@ const eventHandler = new EventHandler(uploader);
 export function activate(context: ExtensionContext): void {
   const subscriptions = context.subscriptions;
 
-
+  Logger.setOutputChannel(window.createOutputChannel('Coding Stats'));
 }
 
 export function deactivate(): void {
